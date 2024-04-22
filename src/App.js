96d908207438501e4af2e50f8fdc2fb7454aee1e
ABHI <mba384420@gmail.com>
2024-04-22 18:57:59 +0530

    image slider added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Accordian from "./components/accordian";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
+import ImageSlider from "./components/image-slider";
 function App() {
   return (
     <div className="App">
       {/*<Accordian></Accordian>*/}
       <RandomColor></RandomColor>
       <StarRating></StarRating>
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      ></ImageSlider>
     </div>
   );
 }
